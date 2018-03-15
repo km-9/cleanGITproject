19b34527048c63603f060dadfd30b4c5e832dd1a
Alexander Michels <michac22@wclive.westminster.edu>
2018-03-15 16:13:45 -0400

    cleaned code

--- mazeRunner/runTest1.cpp
+++ mazeRunner/runTest1.cpp
@@ -23,7 +23,9 @@ double frontVal;
 double leftVal;
 double rightVal;
 
-u_result capture_and_display(RPlidarDriver* drv){
+//gets 360 degree data set from lidar
+u_result capture_and_display(RPlidarDriver* drv)
+{
 	u_result ans;
 	cout << "inside cap_and_disp" << endl;
 
@@ -58,114 +60,157 @@ u_result capture_and_display(RPlidarDriver* drv){
 	return ans;
 }
 
-bool canLeft(){
+bool canLeft()
+{
 	cout << "leftCall" << endl;
 	cout << leftVal << endl;
-  if(leftVal > 200){
+  if(leftVal > 200)
+	{
 //  if(leftCount < 1){
 		cout << "canLeft" << endl;
     return true;
 //  }
-}
+	}
   return false;
 }
 
-bool canRight(){
-cout << "rightCall" << endl;
-cout << rightVal << endl;
-if(rightVal > 200 && !canLeft()){
+bool canRight()
+{
+	cout << "rightCall" << endl;
+	cout << rightVal << endl;
+	if(rightVal > 200 && !canLeft()){
  //if(rightCount < 1){
 	 cout << "canRight" << endl;
    return true;
-// }
-}
+	 // }
+	}
  return false;
 }
 
-bool canForward(){
-		cout << "forwardCall" << endl;
-		cout << frontVal << endl;
-if(frontVal > 175 || frontVal == 0){
+bool canForward()
+{
+	cout << "forwardCall" << endl;
+	cout << frontVal << endl;
+	if(frontVal > 175 || frontVal == 0){
 	cout << "canForward" << endl;
- return true;
-}
+ 	return true;
+	}
  return false;
 }
 
-int main (int argc, char const *argv[]) {
+int main (int argc, char const *argv[])
+{
 	cout << "inside main" << endl;
+	//left wheel
 	PCA9685 pwm1;
 	pwm1.init(1,0x40);
 
+	//right wheel
   PCA9685 pwm2;
 	pwm2.init(1,0x40);
 
+	//look up the documentation on this
 	pwm1.setPWMFreq(61);
   pwm2.setPWMFreq(61);
-	usleep(1000 * 1000);
+
+	//hold for a sec
+	usleep(1000000);
+
+	//stop both wheels 0 and 1 pins
   pwm1.setPWM(0,0,0);
 	pwm2.setPWM(1,0,0);
-    int opt_com_baudrate = 115200;
-    const char * opt_com_path = "/dev/ttyUSB0";
-    RPlidarDriver * drv = RPlidarDriver::CreateDriver(RPlidarDriver::DRIVER_TYPE_SERIALPORT);
-		do {
-        // try to connect
-        if (IS_FAIL(drv->connect(opt_com_path, opt_com_baudrate))) {
-            fprintf(stderr, "Error, cannot bind to the specified serial port %s.\n"
-                , opt_com_path);
-            break;
-        }
-        drv->startMotor();
 
-        // take only one 360 deg scan and display the result as a histogram
-        ////////////////////////////////////////////////////////////////////////////////
-        if (IS_FAIL(drv->startScan( /* true */ ))) // you can force rplidar to perform scan operation regardless whether the motor is rotating
-        {
-            fprintf(stderr, "Error, cannot start the scan operation.\n");
-            break;
-        }
+	//to communicate with lidar
+  int opt_com_baudrate = 115200;
+	//device path for lidar
+  const char * opt_com_path = "/dev/ttyUSB0";
 
-        if (IS_FAIL(capture_and_display(drv))) {
-            fprintf(stderr, "Error, cannot grab scan data.\n");
-            break;
+  RPlidarDriver * drv = RPlidarDriver::CreateDriver(RPlidarDriver::DRIVER_TYPE_SERIALPORT);
+	do {
+    	// try to connect
+      if (IS_FAIL(drv->connect(opt_com_path, opt_com_baudrate)))
+			{
+          fprintf(stderr, "Error, cannot bind to the specified serial port %s.\n", opt_com_path);
+          break;
+      }
+			//spin lidar
+      drv->startMotor();
 
-        }
-				cout << "end of lidar driver in main" << endl;
-			}while(0);
-while(true){
-	capture_and_display(drv);
-	while (canForward()){
-		capture_and_display(drv);
-		pwm1.setPWM(0, 0, 150);
-		pwm2.setPWM(1, 0, 600);
-		usleep(1000 * 1000);
-			if (leftVal > 250){
-				pwm1.setPWM(0,0,0);
-		    pwm2.setPWM(1,0,600);
-				usleep(1000 * 500);
-				//capture_and_display(drv);
-			}
-			if (leftVal < 200){
-				pwm1.setPWM(0, 0, 150);
-				pwm2.setPWM(1, 0, 0);
-				usleep(1000 * 500);
-				//capture_and_display(drv);
-			}
+      // take only one 360 deg scan and display the result as a histogram (we don't know wtf this is?)
+			//check doc
+      if (IS_FAIL(drv->startScan( /* true */ ))) // you can force rplidar to perform scan operation regardless whether the motor is rotating
+      {
+          fprintf(stderr, "Error, cannot start the scan operation.\n");
+          break;
+      }
+
+      if (IS_FAIL(capture_and_display(drv))) {
+          fprintf(stderr, "Error, cannot grab scan data.\n");
+          break;
+      }
+			cout << "end of lidar driver in main" << endl;
 		}
-		if (!canForward){
-		pwm1.setPWM(0, 0, 0);
-		pwm2.setPWM(1, 0, 0);
-		usleep(1000 * 1000);
+		while(0);
 
-		pwm1.setPWM(0, 0, 600);
-		pwm2.setPWM(1, 0, 150);
-		usleep(1000 * 1000);
+	while(true)
+	{
+		//read lidar if initiziatization didn't error out
+		capture_and_display(drv);
 
-		pwm1.setPWM(0, 0, 0);
-		pwm2.setPWM(1, 0, 600);
-		usleep(1000 * 1000);
+		//while we can go forward
+		while (canForward())
+		{
+			//read again
+			capture_and_display(drv);
+
+			//check to see if 150/600 gets straight line
+			pwm1.setPWM(0, 0, 150);
+			pwm2.setPWM(1, 0, 600);
+
+			//hold a sec
+			usleep(1000000);
+
+				//too far from wall
+				//leftVal = d(this, object @ pi/4)
+				if (leftVal > 250)
+				{
+					//turn left
+					pwm1.setPWM(0,0,0);
+			    pwm2.setPWM(1,0,600);
+					usleep(500000);
+					//capture_and_display(drv);
+				}
+
+				//too close to wall
+				if (leftVal < 200)
+				{
+					//turn right
+					pwm1.setPWM(0, 0, 150);
+					pwm2.setPWM(1, 0, 0);
+					usleep(500000);
+					//capture_and_display(drv);
+				}
+			}
+
+		if (!canForward)
+		{
+			//why isn't this stopping?
+			pwm1.setPWM(0, 0, 0);
+			pwm2.setPWM(1, 0, 0);
+			usleep(1000000);
+
+
+			//reverses
+			pwm1.setPWM(0, 0, 600);
+			pwm2.setPWM(1, 0, 150);
+			usleep(1000000);
+
+			//turn left (left wall following)
+			pwm1.setPWM(0, 0, 150);
+			pwm2.setPWM(1, 0, 0);
+			usleep(1000000);
+		}
 	}
-}
     drv->stop();
     drv->stopMotor();
 
