73c0bf06eda590984438300ba5795dcbbce1ed32
Unknown <kylemonteleone96@gmail.com>
2018-03-16 23:23:31 -0700

    No more ->
    all is . now

--- mazeRunner/wallFollower.cpp
+++ mazeRunner/wallFollower.cpp
@@ -14,36 +14,36 @@ namespace WallFollower
         pwm2.init(1,0x40);
 
         //look up the documentation on this
-        pwm1->setPWMFreq(61);
-        pwm2->setPWMFreq(61);
+        pwm1.setPWMFreq(61);
+        pwm2.setPWMFreq(61);
 
         //hold for a sec
         usleep(1000000);
 
         //stop both wheels 0 and 1 pins
-        pwm1->setPWM(0,0,0);
-        pwm2->setPWM(1,0,0);
+        pwm1.setPWM(0,0,0);
+        pwm2.setPWM(1,0,0);
 
         //to communicate with lidar
         int opt_com_baudrate = 115200;
       	//device path for lidar
         const char * opt_com_path = "/dev/ttyUSB0";
 
-        this->drv = RPlidarDriver::CreateDriver(RPlidarDriver::DRIVER_TYPE_SERIALPORT);
+        this.drv = RPlidarDriver::CreateDriver(RPlidarDriver::DRIVER_TYPE_SERIALPORT);
       	do
         {
           	// try to connect
-            if (IS_FAIL(drv->connect(opt_com_path, opt_com_baudrate)))
+            if (IS_FAIL(drv.connect(opt_com_path, opt_com_baudrate)))
       			{
                 fprintf(stderr, "Error, cannot bind to the specified serial port %s.\n", opt_com_path);
                 break;
             }
       			//spin lidar
-            drv->startMotor();
+            drv.startMotor();
 
             // take only one 360 deg scan and display the result as a histogram (we don't know wtf this is?)
       			//check doc
-            if (IS_FAIL(drv->startScan( /* true */ ))) // you can force rplidar to perform scan operation regardless whether the motor is rotating
+            if (IS_FAIL(drv.startScan( /* true */ ))) // you can force rplidar to perform scan operation regardless whether the motor is rotating
             {
                 fprintf(stderr, "Error, cannot start the scan operation.\n");
                 break;
@@ -58,10 +58,10 @@ namespace WallFollower
       	while(0);
         cout << "in wallFollower(drv)" << endl;
         //I incremented these because hypotenuses now
-        this->min_forward_dist = 300;
-        this->min_left_dist = 300;
-        this->min_right_dist = 300;
-        this->max_left_dist = 400;
+        this.min_forward_dist = 300;
+        this.min_left_dist = 300;
+        this.min_right_dist = 300;
+        this.max_left_dist = 400;
         updateDists();
     }
 
@@ -138,25 +138,25 @@ namespace WallFollower
 
     void wallFollower::goForward()
     {
-      pwm1->setPWM(0, 0, 150);
-			pwm2->setPWM(1, 0, 600);
+      pwm1.setPWM(0, 0, 150);
+			pwm2.setPWM(1, 0, 600);
     }
 
     void wallFollower::pause(int units)
     {
-      drv->usleep(units);
+      drv.usleep(units);
     }
 
     void wallFollower::reverse()
     {
-      pwm1->setPWM(0, 0, 600);
-			pwm2->setPWM(1, 0, 150);
+      pwm1.setPWM(0, 0, 600);
+			pwm2.setPWM(1, 0, 150);
     }
 
     void wallFollower::stop()
     {
-      pwm1->setPWM(0,0,0);
-    	pwm2->setPWM(1,0,0);
+      pwm1.setPWM(0,0,0);
+    	pwm2.setPWM(1,0,0);
     }
 
     void wallFollower::takeItBackNowYall()
@@ -182,15 +182,15 @@ namespace WallFollower
       //if this doesn't work I would want to make time our parameter
       //and for (i < units) turn, then take in a bool keep_moving
       //if keep_moing == true go back to straight, else stop
-      pwm1->setPWM(0,0,100);
-      pwm2->setPWM(1,0,600);
+      pwm1.setPWM(0,0,100);
+      pwm2.setPWM(1,0,600);
     }
 
     void wallFollower::strafeRight(int degrees)
     {
       //same idea with turnLeft()
-      pwm1->setPWM(0,0,150);
-      pwm2->setPWM(1,0,550);
+      pwm1.setPWM(0,0,150);
+      pwm2.setPWM(1,0,550);
     }
 
     void wallFollower::updateDists()
@@ -200,10 +200,10 @@ namespace WallFollower
     	rplidar_response_measurement_node_t nodes[360*2];
     	int count = _countof(nodes);
 
-    	ans = drv->grabScanData(nodes, count);
+    	ans = drv.grabScanData(nodes, count);
     	if (IS_OK(ans) || ans == RESULT_OPERATION_TIMEOUT)
     	{
-    			drv->ascendScanData(nodes, count);
+    			drv.ascendScanData(nodes, count);
 
     			double tmp [360];
     			for (int pos = 0; pos < (int)count ; ++pos)
@@ -215,7 +215,7 @@ namespace WallFollower
     			printf("error code: %x\n", ans);
     	}
 
-      this->dists = tmp;
+      this.dists = tmp;
     }
 
     void wallFollower::turnLeftDegree(int degrees){
