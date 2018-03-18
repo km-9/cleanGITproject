ef607392d99e28b989b9df1a7dac9f1c92c67393
ConnorB123 <connor.j.byrnes@gmail.com>
2018-03-18 11:31:44 -0400

    test of left wall following

--- mazeRunner/wallFollowerTest1.cpp
+++ mazeRunner/wallFollowerTest1.cpp
@@ -6,11 +6,33 @@ using namespace std;
 int main (int argc, char const *argv[])
 {
   wallFollower f = wallFollower();
-  cout << "constructor finished" << endl;
-  f.goForward();
-  cout << "goForward finished" << endl;
-  f.holdTheFuckUp(10000000);
-  cout << "holdTheFuckUp" << endl;
+  //cout << "constructor finished" << endl;
+  //f.goForward();
+  //cout << "goForward finished" << endl;
+  //f.holdTheFuckUp(10000000);
+  //cout << "holdTheFuckUp" << endl;
+
+  f.max_left_dist = 300;
+  f.min_forward_dist = 150;
+
+
+  while(true) {
+    if(f.canGoForward() && !f.tooFarOnLeft()) {
+      f.goForward();
+    }
+    else if(f.canGoForward() && f.tooFarOnLeft()) {
+      f.strafeLeft();
+    }
+    else if(!f.canGoForward() && f.canGoLeft()) {
+      f.turnLeft(90);
+    }
+    else if(!f.canGoForward() && !f.canGoLeft() && f.canGoRight()) {
+      f.turnright(90);
+    }
+    else {
+      f.turnleft(180);
+    }
+  }
 
   /*while (true)
   {
