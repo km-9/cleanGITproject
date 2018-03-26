637bad489cfafb3f225d748be83f88477646aab8
Unknown <kylemonteleone96@gmail.com>
2018-03-26 14:38:34 -0700

    namespace problems

--- wallFollower.cpp
+++ wallFollower.cpp
@@ -708,7 +708,7 @@ namespace WallFollower
       //turnRight(35);
     }
 
-    void camToFile(string fileName)
+    void  cv::camToFile(string fileName)
     {
   // Create a VideoCapture object and use camera to capture the video
   VideoCapture cap(0);
