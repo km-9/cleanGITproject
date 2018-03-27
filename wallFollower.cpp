7f1b2be91ec37bfb13e343018d5ce81688964622
ConnorB123 <connor.j.byrnes@gmail.com>
2018-03-27 15:13:32 -0400

    front value is very large now

--- wallFollower.cpp
+++ wallFollower.cpp
@@ -696,7 +696,7 @@ namespace WallFollower
     }
     //Front Wall Handler
     void wallFollower::frontHandler(double fAvg, double lAvg){
-      if(fAvg < 750) {
+      if(fAvg < 950) {
         while(getDists(180) < 275) {
           turnRightInPlace();
           updateDists();
