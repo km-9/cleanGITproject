a0abed30bea0c302ab458927c9a415e571464954
Unknown <kylemonteleone96@gmail.com>
2018-03-27 13:25:33 -0700

    all in main

--- wallFollowerTest1.cpp
+++ wallFollowerTest1.cpp
@@ -1,10 +1,12 @@
 #include "wallFollower.h"
 #include <stdlib.h>
-#include <iostream>
 #include <fstream>
+#include "opencv2/opencv.hpp"
+#include <iostream>
 
 using namespace WallFollower;
 using namespace std;
+using namespace cv;
 
 bool canLeft;
 bool canRight;
@@ -20,7 +22,37 @@ int main (int argc, char const *argv[])
   //f.holdTheFuckUp(10000000);
   //cout << "holdTheFuckUp" << endl;
 // TODO f.startWallFollowingLeft();
-f.camToFile();
+//***********************************************
+  // Create a VideoCapture object and use camera to capture the video
+  VideoCapture cap(0);
+
+  // Check if camera opened successfully
+  if(!cap.isOpened())
+  {
+    cout << "Error opening video stream" << endl;
+    return -1;
+  }
+
+  // Default resolution of the frame is obtained.The default resolution is system dependent.
+  int frame_width = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+  int frame_height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+
+  // Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file.
+  VideoWriter video("outcpp.avi",CV_FOURCC('M','J','P','G'),10, Size(frame_width,frame_height));
+  while(1)
+  {
+    Mat frame;
+
+    // Capture frame-by-frame
+    cap >> frame;
+
+    // If the frame is empty, break immediately
+    if (frame.empty())
+      break;
+
+    // Write the frame into the file 'outcpp.avi'
+    video.write(frame);
+//***********************************************
 while(true){
   f.updateDists();
   //Left Vals
@@ -40,7 +72,25 @@ while(true){
   f.leftWallFollower(leftAvg);
 }
 
-//Closes thing to working//Closes thing to working
+
+    // Display the resulting frame
+    imshow( "Frame", frame );
+
+    // Press  ESC on keyboard to  exit
+    char c = (char)waitKey(1);
+    if( c == 27 )
+      break;
+  }
+
+  // When everything done, release the video capture and write object
+  cap.release();
+  video.release();
+
+  // Closes all the windows
+  destroyAllWindows();
+  return 0;
+
+/*Closes thing to working//Closes thing to working
 while (true){
     usleep(10000);
     f.updateDists();
