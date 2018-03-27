5b20fce5b4faf171d1d599c68255df5a28eafd3d
ConnorB123 <connor.j.byrnes@gmail.com>
2018-03-27 16:05:28 -0400

    changed values

--- wallFollower.cpp
+++ wallFollower.cpp
@@ -702,7 +702,7 @@ namespace WallFollower
           turnRightInPlace();
           updateDists();
         }
-        while(getDists(80) < getDists(90)) {
+        while(getDists(70) < getDists(90)) {
           turnRightInPlace();
           updateDists();
         }
