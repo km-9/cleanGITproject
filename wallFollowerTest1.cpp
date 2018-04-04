02e8fbb925490fc44866736071489a255be6552a
Unknown <kylemonteleone96@gmail.com>
2018-04-03 22:40:31 -0700

    better boxes

--- wallFollowerTest1.cpp
+++ wallFollowerTest1.cpp
@@ -187,43 +187,21 @@ Mat findFire(Mat frame){
 
   bitwise_and(frame, postColor, output, mask);
 
-  drawRect(frame);
-
   return output;
 }
 
 Rect drawRect(Mat src){
- cvtColor( src, src, COLOR_BGR2GRAY );
- Mat threshold_output;
- vector<vector<Point> > contours;
- vector<Vec4i> hierarchy;
-
- threshold( src, threshold_output, thresh, 255, THRESH_BINARY );
- findContours( threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
-
- vector<vector<Point> > contours_poly( contours.size() );
- vector<Rect> boundRect( contours.size() );
- vector<Point2f>center( contours.size() );
-
- vector<float>radius( contours.size() );
- for( size_t i = 0; i < contours.size(); i++ )
+ Mat plain;
+ cvtColor(src, plain, CV_BGR2GRAY);
+ vector<cv::Point> points;
+ Mat_<uchar>::iterator it = plain.begin<uchar>();
+ Mat_<uchar>::iterator end = plain.end<uchar>();
+ for (; it != end; ++it)
  {
-   approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
-   boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-   minEnclosingCircle( contours_poly[i], center[i], radius[i] );
+     if (*it) points.push_back(it.pos());
  }
+ RotatedRect box = cv::minAreaRect(cv::Mat(points));
 
- Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
-
- for( size_t i = 0; i< contours.size(); i++ )
- {
-   Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-   drawContours( drawing, contours_poly, (int)i, color, 1, 8, vector<Vec4i>(), 0, Point() );
-   rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
-   circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
- }
- Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
- return rectangle( drawing, boundRect[contours.size()-1].tl(), boundRect[contours.size()-1].br(), color, 2, 8, 0 );
 
  //namedWindow( "Contours", WINDOW_NORMAL );
  //resizeWindow("Contours", 500,500);
