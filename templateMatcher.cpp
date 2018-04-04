b4e459669f5220a198732487112152e76abeef47
Unknown <kylemonteleone96@gmail.com>
2018-04-03 21:12:50 -0700

    odd

--- templateMatcher.cpp
+++ templateMatcher.cpp
@@ -30,55 +30,53 @@ with minmal difficulty and realative accuracy. (an offset of a few degrees/cm is
 highly likely)
 */
 
+// open the default camera (we only want one)
+VideoCapture cap(0);
+
+//if the matching method uses a mask or not
+bool use_mask;
+
+//img: the frame captured from the camera
+Mat img;
+
+//templ: current template being used by matching method
+Mat templ;
+
+//mask: mask for template (if any)
+Mat mask;
+
+//result: the resulting image after processing
+Mat result;
+
+//template of left side of crib
+Mat leftTempl;
+
+//right side of crib
+Mat rightTempl;
+
+//face/front of crib
+Mat frontTempl;
+
+//scaling multiplyer for template
+double scale;
+
+//determines which side of the crib is being seen (according to which template is being used)
+int side;
+
+//always set to 4.
+int match_method;
+
+//calls getFrame(), checks if it was successful and then calls MatchingMethod;
+void lookAround();
+
+//runs the matching method and processes the image
+void MatchingMethod( int, void* );
+
+//returns a frame from a video stream
+Mat getFrame();
+
 class templateMatcher
 {
-public:
-
-  // open the default camera (we only want one)
-  VideoCapture cap(0);
-
-  //if the matching method uses a mask or not
-  bool use_mask;
-
-  //img: the frame captured from the camera
-  Mat img;
-
-  //templ: current template being used by matching method
-  Mat templ;
-
-  //mask: mask for template (if any)
-  Mat mask;
-
-  //result: the resulting image after processing
-  Mat result;
-
-  //template of left side of crib
-  Mat leftTempl;
-
-  //right side of crib
-  Mat rightTempl;
-
-  //face/front of crib
-  Mat frontTempl;
-
-  //scaling multiplyer for template
-  double scale;
-
-  //determines which side of the crib is being seen (according to which template is being used)
-  int side;
-
-  //always set to 4.
-  int match_method;
-
-  //calls getFrame(), checks if it was successful and then calls MatchingMethod;
-  void lookAround();
-
-  //runs the matching method and processes the image
-  void MatchingMethod( int, void* );
-
-  //returns a frame from a video stream
-  Mat getFrame();
-
   templateMatcher(){
     leftTempl = imread( "/home/pi/Downloads/lidar_sdk/sdk/app/images/Left.jpg", IMREAD_COLOR );
     rightTempl = imread( "/home/pi/Downloads/lidar_sdk/sdk/app/images/Right.jpg", IMREAD_COLOR );
