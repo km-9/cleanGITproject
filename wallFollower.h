2a29f3dd947bbf5cf72e951fd59fa4fc662ff713
km-9 <kylemonteleone96@gmail.com>
2018-04-05 22:52:23 +0000

    Cleaned up code and tweaked wall-following logic

--- wallFollower.h
+++ wallFollower.h
@@ -27,12 +27,6 @@ namespace WallFollower
   class wallFollower
   {
       public:
-         //array of past canGoLeft values
-         bool * pastGoLefts;
-
-         //array of past canGoRight values
-         bool * pastGoRights;
-
           //an 360 length array with distance from a lidar detector to the nearest detectable object at the index's angle
           double dists [360];
 
@@ -43,24 +37,7 @@ namespace WallFollower
           std::ofstream logfile;
 
           //left wheel
-          PCA9685 pwm1;
-
-          //right wheel
-          PCA9685 pwm2;
-
-          int avoidance_radius;
-
-          //maximum allowable distance between the robot and the wall
-          int max_left_dist;
-
-          //minimum allowable distance between the robot and any object in front of it
-          int min_forward_dist;
-
-          //minimum allowable distance between the robot and any object to the left of it
-          int min_left_dist;
-
-          //minimum allowable distance between the robot and any object to the right of it
-          int min_right_dist;
+          PCA9685 pwm;
 
           //constructor
           wallFollower();
@@ -68,124 +45,14 @@ namespace WallFollower
           //averages distances between degree values, returns -1 if all values in the range are zero
           double avg(int start, int end);
 
-          //dka;hghd
-          int avoidHeadOn();
-
-          //scans front Left quadrant for obstacles
-          bool avoidLeft();
-
-          //scans front Right quadrant for obstacles
-          bool avoidRight();
-
-          //checks if the robot is at an intersection
-          bool beSmart();
-
-          //returns whether or not we are able to reverse
-          bool canGoBack();
-
-          //returns whether or not we are able to go forward
-          bool canGoForward();
-
-          //returns whether or not we are able to go left
-          bool canGoLeft();
-
-         //return whether or not we are able to go right
-         bool canGoRight();
-
-         //whether or not the majority of past canGoLeft values were true
-         bool couldntGoLeft();
-
-         //whether or not the majority of past canGoRight values were true
-         bool couldntGoRight();
-
-         //handler call for front collisions
-         bool frontHandler(double fAvg, double lAvg);
-
          //getter for dist[i]
          double getDists(int i);
-
-         //getter for max_left_dist
-         int getMaxLeftDist();
-
-         //getter for min_forward_dist
-         int getMinForwardDist();
-
-         //getter for min_left_dist
-         int getMinLeftDist();
-
-         //getter for min_right_dist
-         int getMinRightDist();
-
-         //makes the robot go forward hopefully
-         void goForward();
-
-         //calls pause(units)
-         void holdTheFuckUp(int units);
-
-         //majority of past canGoLeft values were false, but we now canGoLeft()
-         bool leftPathAppeared();
-
-         //takes an average distance from the wall and follows it
-         bool leftWallFollower(double avg);
-
-         //uses avg of forward-left distances to orient robot to a wall
-         bool orientationFix(double leftAngleAvg, double lAvg);
-
-          //holds for units milliseconds?
-          void pause(int units);
-
-          //reverses the robot
-          void reverse();
-
-          //majority of past canGoRight values were false, but now we canGoRight()
-          bool rightPathAppeared();
-
-          //returns whether or not the robot should strafe left to align with the left wall
-          bool strafeLeftToAlignWithWall();
-
-          //returns whether or not the robot should strafe right to align with the right wall
-          bool strafeRightToAlignWithWall();
-
-          //start wall following left
-          void startWallFollowingLeft();
 
           //stop both wheels
           void stop();
 
-          //arc left significantly
-          void strafeLeft();
-
-          //arc right significantly
-          void strafeRight();
-
-          //slightly sway towards left
-          void swayToLeft();
-
-          //slightly tend towards right
-          void swayToRight();
-
-          void turnRightInPlace();
-
-          //calls reverse()
-          void takeItBackNowYall();
-
-          //returns whether or not the robot is too far from the wall on the left
-          bool tooFarOnLeft();
-
-          //turns the robot degrees to the left
-          void turnLeft(int degrees);
-
-          //turns left until can't go left
-          void turnLeftUntilYouCant();
-
-          //turns the robot degree so the right
-          void turnRight(int degres);
-
           //updates the dist[] using drv
           void updateDists();
-
-          //getVideoCaptureToFile
-          void camToFile();
 
 	  // set the left and right motor speeds speeds range from
 	  // -128 to 128 where negative speeds are backwards.  The
--- wallFollowerTest1.cpp
+++ wallFollowerTest1.cpp
@@ -53,23 +53,24 @@ void frontCaseHandler() {
   f.setMotorSpeed(0,0);
   pause(.5);
   f.setMotorSpeed(20, -20);
-  pause(.5);
+  pause(.6);
   f.setMotorSpeed(0,0);
 }
 
 void checkBumpSwitch() {
 	if(!digitalRead(7)) {
 		f.stop();
+		pause(.5);
+		f.setMotorSpeed(-10,20);
+		pause(.5);
+		f.stop();
 	}
 }
 
 int main(int argc, char const *argv[]) {
-  cap >> frame;
-  frame = findFire(frame);
-  drawRect(frame);
 
   wiringPiSetup();
-  pinMode(4, INPUT);
+  pinMode(7, INPUT);
 
   while(true) {
     f.updateDists();
@@ -91,173 +92,3 @@ int main(int argc, char const *argv[]) {
   }
 }
 
-int mainqqqqq (int argc, char const *argv[])
-{
-  cout << "in main" << endl;
-//***********************************************
-
-  // Check if camera opened successfully
-  if(!cap.isOpened())
-  {
-    cout << "Error opening video stream" << endl;
-    return -1;
-  }
-
-  // Default resolution of the frame is obtained.The default resolution is system dependent.
-  int frame_width = cap.get(CV_CAP_PROP_FRAME_WIDTH);
-  int frame_height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
-
-  // Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file.
-  VideoWriter video("outcppJUNK.avi",CV_FOURCC('M','J','P','G'),10, Size(frame_width,frame_height));
-
-  const char* source_window = "Source";
-  namedWindow( source_window, WINDOW_NORMAL );
-  resizeWindow("souce", 500,500);
-  Mat temp;
-  cap >> temp;
-  imshow( source_window, temp );
-  //createTrackbar( " Threshold:", "Source", &thresh, max_thresh, drawRect);
-  //trackTarget(drawRect(findFire(temp)));
-
-//***********************************************
-while(true){
-  Mat capFrame;
-  f.updateDists();
-  getAvg();
-  //*****************
-  // Capture frame-by-frame
-  cap >> capFrame;
-
-  // If the frame is empty, break immediately
-  if (capFrame.empty())
-    break;
-
-  // Write the frame into the file 'outcpp.avi'
-  video.write(capFrame);
-  //*****************
-  cout<<left<<"|Left"<<endl;
-  cout<<softLeft<<"|softLeft"<<endl;
-  cout<<frontLeft<<"|frontLeft"<<endl;
-  cout<<leftFront<<"|leftFront"<<endl;
-  cout<<front<<"|Front"<<endl;
-  cout<<"--------------------------"<<endl;
-
-  /*
-  The chain of decision making should be as follows.
-  1| Collision Avoidance (if the robot is on a collision course it must stop itself and adjust)
-  2| Orientation/Turning (the robot must be oriented properly in regards to the wall)
-  3| Distance (the robot must maintain a proper distance from obstacles)
-  4| Movement (the robot should move forward under basic principles)
-  *wildcard*| Decision Making (the robot must make decisions based upon conditions, the nature of these determines their ordering in the heirarchy)
-  */
-
-  /*if (wontCrash){
-    if (wontSkew){
-      if (notTooShort/Long){
-        if (canMoveForward){
-
-        }
-      }
-    }
-  }*/
-
-
-
-  if (front < 200 && front != 0){
-    while (front < 300 && front != 0){
-      f.stop();
-      cout<<"cant go forward"<<endl;
-      f.updateDists();
-      front = f.avg(175, 185);
-    }
-    while (realLeft > front){
-      f.updateDists();
-      f.turnRightInPlace();
-      front = f.avg(175, 185);
-      realLeft = f.avg(87, 93);
-    }
-  }
-  else if (realLeft > softLeft){
-    f.strafeRight();
-  }
-  else if (softLeft > frontLeft){
-    f.strafeRight();
-  }
-  else if (frontLeft < realLeft){
-    f.strafeRight();
-  }
-  else if (realLeft > 200 && frontLeft-50>realLeft){
-    f.strafeLeft();
-  }
-  //all is well. Make most basic wallfollowing decisions
-  else if (realLeft < 200){
-    f.swayToRight();
-  }
-  else if (realLeft > 200){
-    f.swayToLeft();
-  }
-  else if (realLeft == 200){
-    f.goForward();
-  }
-
-
-}
-  // When everything done, release the video capture and write object
-  cap.release();
-  video.release();
-  f.drv->stop();
-  f.drv->stopMotor();
-
-  rp::standalone::rplidar::RPlidarDriver::DisposeDriver(f.drv);
-
-  return 0;
-}
-
-void getAvg(){
-  //Left Direct Avg
-  realLeft = f.avg(87, 93);
-  //General realLeft Avg
-  softLeft = f.avg(90, 135);
-  //Front realLeft Avg
-  frontLeft = f.avg(125, 160);
-  //Left Front Avg
-  leftFront = f.avg(145, 170);
-  //Front Direct Avg
-  front = f.avg(175, 185);
-  //Right Front Avg
-  rightFront = f.avg(190, 215);
-  //Front Right Avg
-  frontRight = f.avg(210, 235);
-  //General realLeft Avg
-  softRight = f.avg(225, 270);
-}
-
-//TODO this all needs to get redone into proper OOP style
-Mat findFire(Mat frame){
-  Size_<int> size;
-  size = Size_<int>(21, 21);
-
-  GaussianBlur(frame, blurFrame, size, 0, 0, BORDER_DEFAULT);
-  return blurFrame;
-}
-
-Rect drawRect(Mat src){
- double minVal1; double maxVal1; Point minLoc1; Point maxLoc1;
- Mat plain;
- cvtColor(src, plain, CV_BGR2GRAY);
- minMaxLoc( plain, &minVal1, &maxVal1, &minLoc1, &maxLoc1, Mat() );
-cout<<maxVal1<<" maxVal"<<endl;
-cout<<maxLoc1.x << " max x loc"<<endl;
-Rect box;
-if (maxVal1 > 100){
- box.x = (maxLoc1.x - 10);
- box.y = (maxLoc1.y - 10);
- box.width = 20;
- box.height = 20;
-}
- rectangle(src, box, CV_BGR2GRAY, 1, 8, 0);
- cout<<"got to imshow"<<endl;
- imshow("box", src);
-
- return box;
-}
